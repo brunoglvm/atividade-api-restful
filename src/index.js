6a0a1b086e3cffb9194d110698a90a0616114fac
Muniz <ccmuniz2003@gmail.com>
2024-10-23 19:56:38 -0300

    Adicionado Post e Get

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import router from "./routes.js";
+const {create, findAll} = require('./repositories/repository.alunos.js')
+
 
 const app = express();
 const port = 3001;
@@ -8,6 +10,17 @@ const port = 3001;
 app.use(cors());
 app.use(router);
 
+//post
+app.post('/alunos', (req,res)=>{
+  const {nome,email,nome_curso} = req.body
+  const vaga = create({nome, email, nome_curso})
+  res.status(201).json(alunos)
+})
+
+app.get('/alunos', (req,res)=>{
+  const alunos = findAll()
+  res.json(alunos)
+})
 
 app.listen(3001, () => {
   console.log(`Servidor rodando na porta ${port}`);
--- src/repositories/repository.alunos.js
+++ src/repositories/repository.alunos.js
@@ -1,3 +1,26 @@
+const {v4: uuidv4} = require('uuid')
 
 const alunos = [];
 
+//criar
+function create(descricao, titulo, dataCadastro, telefone, empresa) {
+    const vaga = {
+        id: uuidv4(),
+        nome,
+        email,
+        nome_curso,
+    }
+
+    alunos.push(alunos)
+    return alunos
+}
+
+//listar
+function findAll(){
+    return alunos
+}
+
+module.exports = {
+    create,
+    findAll
+}
