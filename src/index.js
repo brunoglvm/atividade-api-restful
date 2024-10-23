f497044c9d3cf1f67d8d30e9261b99da550b2aed
Bruno Galvão <bgalvaomiranda302@gmail.com>
2024-10-22 21:07:34 -0300

    add start script

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 const app = express();
 app.use(cors());
 
-app.listen(3001, () =>{
-    console.log('Servidor rodando na porta 3001');
-})
+app.listen(3001, () => {
+  console.log("Servidor rodando na porta 3001");
+});
