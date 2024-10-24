d24a6a3d96ba6a5a309309878861217f1f70f57c
Bruno Galvão <bgalvaomiranda302@gmail.com>
2024-10-24 15:53:24 -0300

    feat: add Helmet

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
 import router from "./routes.js";
 
 const app = express();
@@ -8,8 +9,9 @@ const port = 3001;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(helmet());
 app.use(router);
 
 app.listen(port, () => {
-  console.log(`O Servidor está rodando na porta ${port}`);
+  console.log(`Servidor rodando em: http://localhost:${port}/alunos`);
 });
